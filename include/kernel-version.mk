ca793c25d5cf3bd7545f4bfaa2277fb192f806bf
Russell Senior <russell@personaltelco.net>
2018-09-11 03:00:09 -0700

    WIP: port to kernel 4.18.x
    Signed-off-by: Russell Senior <russell@personaltelco.net>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -5,10 +5,12 @@ LINUX_RELEASE?=1
 LINUX_VERSION-3.18 = .121
 LINUX_VERSION-4.9 = .125
 LINUX_VERSION-4.14 = .68
+LINUX_VERSION-4.18 = .7
 
 LINUX_KERNEL_HASH-3.18.121 = 7a8a996433e4af1abd2903f91c390295745d90dd178607b3af82f2df5f95c9b0
 LINUX_KERNEL_HASH-4.9.125 = a764deef61bebfac1d07b2ed6890f93a12a9ab6d3fc3c53e3d850ed4681111cb
 LINUX_KERNEL_HASH-4.14.68 = 791dbf1597033bf2b61e83307d78188ffc1ad4bdd1da3234876667edfdd28690
+LINUX_KERNEL_HASH-4.18.7 = f03b425e262a71e5079736706233a4e9afaf77c8462b552b4d6db2d33f5af731
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
